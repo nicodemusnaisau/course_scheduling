ba7851eab121734dbcf0fbd8e504f752c4b5bb67
nicodemusnaisau <70936890+nicodemusnaisau@users.noreply.github.com>
2022-10-16 11:48:28 +0700

    matlab genetic al

---
+++ GeneticAlgorithm.m
@@ -0,0 +1,10 @@
+clear
+clc
+
+target = input('Target (string) : ','s');
+population = input('Population (integer) : ');
+mutation_rate = input('Mutation rate random(0-1) : ');
+
+% Liblary Gentic Algorithms GA(taeget,populations,mutation)
+
+GA(target,population,mutation_rate);
